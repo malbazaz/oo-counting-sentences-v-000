71ac024097aef732a571dc63903dee069aa32958
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-17 18:22:17 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+  if self.include?(".")
+    true 
+  else 
+    false 
+  end 
   end
 
   def question?
-
+if self.end_with?("?")
+  true 
+else 
+  false 
+end 
   end
 
   def exclamation?
-
+if self.end_with?("!")
+  true 
+else
+  false 
+end 
   end
 
   def count_sentences
+
+self.squeeze(".!?").split(/[.?!]/).size
+
 
   end
 end
